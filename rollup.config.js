a9325d31a570344c6adb17a28adea22913921663
n1ru4l <laurinquast@googlemail.com>
2021-02-22 18:19:14 +0100

    feat: introduce esm and cjs support according to https://github.com/d3/d3/issues/3469#issue-809713670

---
+++ rollup.config.js
@@ -0,0 +1,27 @@
+import ts from '@rollup/plugin-typescript'
+import { promises as fs } from "fs"
+
+const isCJSBuild = process.env.MODE === "cjs" 
+
+const commonjsPkgJSONPlugin = () => {
+  return {
+    name: 'commonjsPkgJSONPlugin',
+    write: async  () => {
+      await fs.writeFile("dist/cjs/package.json", JSON.stringify({
+        "type": "commonjs"
+      }))
+    }
+  }
+}
+
+export default {
+  input: ['src/index.ts', 'src/relay.ts'],
+  output: [
+    {
+      dir: isCJSBuild ? 'dist/cjs' : 'dist',
+      format: isCJSBuild ? 'cjs' : 'esm'
+    }
+  ],
+  plugins: [ts({ tsconfig: isCJSBuild ? "tsconfig.cjs.json" : "tsconfig.json" }), isCJSBuild ? commonjsPkgJSONPlugin() : undefined].filter(Boolean),
+  external: ["graphql"]
+}
