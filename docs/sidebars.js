d8bf07a760f4e3dfdc0cffa0c6c8af90787ab713
Federico Biccheddu <federico@biccheddu.dev>
2021-02-28 15:38:09 +0100

    chore: update sidebar file

--- docs/sidebars.js
+++ docs/sidebars.js
@@ -1,4 +1,15 @@
 module.exports = {
   sidebar: {
+    'gqtx': [
+      'introduction',
+      'installation',
+      'why',
+    ],
+    'Features': [
+      'feature/queries',
+      'feature/mutations',
+      'feature/subscriptions',
+      'feature/enums'
+    ],
   },
 };
